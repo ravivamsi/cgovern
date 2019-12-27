2ebb427f1ead550f08676f58b479204960ea1238
Vamsi Ravi <ravivamsi@yahoo.com>
2019-12-27 16:29:18 -0600

    Rest Call Implementation In Progress

--- src/App.js
+++ src/App.js
@@ -1,27 +1,72 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
+
+
+class App extends Component{
+
+  state={
+    isLoading: true,
+    projects:[]
+  }
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  async componentDidMount(){
 
 
-      </header>
-      <body>
+    const response = await axios.get("http://jsonplaceholder.typicode.com/users",
+        { headers: {'Content-Type': 'application/json'}}
+      );
+      console.log(response);
+    const body = await response.data;
+    this.setState({
+      projects: body,
+      isLoading: false
+    });
+  }
+
+
+    render(){
+      const {projects, isLoading} = this.state;
+
+      if(isLoading){
+        return <p>Loading .... </p>;
+      }
+
+    return (
+
+
+      <div className="App">
+      <div className="header" >
+          <h3 className="text-muted" align="left">CGovern</h3>
+      </div>
+
+        <header className="App-header">
+
+          <div className="App-intro">
+          <h2>Projects</h2>
+
+          
+          {projects.map(project=>
+              <div key={project.id}>
+              {project.name}
+              </div>
+          )}
+          </div>
+        </header>
+        <footer class="footer">
+            <p>&copy; 2020 Vamsi Ravi</p>
+        </footer>
+      </div>
 
 
 
 
+    );
+  }
 
-      </body>
 
-
-
-    </div>
-  );
 }
 
 export default App;
